cbf9ec74401b12799abd0fad4e1e03d5fb7cf99b
Vladislav Khurs <130601125+VladKhurs@users.noreply.github.com>
2024-01-15 13:52:30 +0200

    feat: add functionality

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+fs.mkdir(path.join(__dirname, 'files-copy'),
+    { recursive: true },
+    (err) => {
+        if (err) {
+            return console.error(err);
+        }
+});
+const filePath = path.join(__dirname, '/files');
+const filePathTarget = path.join(__dirname, '/files-copy');
+fs.readdir(filePath, (err, files) => { 
+    if (err) {
+        console.log(err); 
+    }
+    else { 
+        files.forEach(file => { 
+            fs.copyFile(path.join(filePath, file), path.join(filePathTarget, file), (err) => {
+              if (err) {
+                  console.log("Error Found:", err);
+              }
+          });
+        }) 
+    } 
+})
